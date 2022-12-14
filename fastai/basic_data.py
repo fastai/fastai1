bd69e46a4869b55dd160643e0374c644b35a2d49
vikashranjan26 <ranjanvikash26@gmail.com>
2022-12-14 11:58:57 +0530

    added kwargs in intercept_args

--- fastai/basic_data.py
+++ fastai/basic_data.py
@@ -9,10 +9,10 @@ old_dl_init = torch.utils.data.DataLoader.__init__
 
 def intercept_args(self, dataset, batch_size=1, shuffle=False, sampler=None, batch_sampler=None,
                  num_workers=0, collate_fn=default_collate, pin_memory=True, drop_last=False,
-                 timeout=0, worker_init_fn=None):
+                 timeout=0, worker_init_fn=None, **kwargs):
     self.init_kwargs = {'batch_size':batch_size, 'shuffle':shuffle, 'sampler':sampler, 'batch_sampler':batch_sampler,
                         'num_workers':num_workers, 'collate_fn':collate_fn, 'pin_memory':pin_memory,
-                        'drop_last': drop_last, 'timeout':timeout, 'worker_init_fn':worker_init_fn}
+                        'drop_last': drop_last, 'timeout':timeout, 'worker_init_fn':worker_init_fn, **kwargs}
     old_dl_init(self, dataset, **self.init_kwargs)
 
 torch.utils.data.DataLoader.__init__ = intercept_args
